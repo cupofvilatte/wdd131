4ab68c8fd92ebc1cbb4e32dcc92bd2357efc283f
cupofvilatte <kna22003@byui.edu>
2024-05-04 17:44:05 -0600

    added js to allow dark mode

--- mission/mission.js
+++ mission/mission.js
@@ -1,1 +1,16 @@
-document.addEventListener()
+document.addEventListener('DOMContentLoaded', function () {
+    const selectElement = document.getElementById('theme');
+
+    selectElement.addEventListener('change', function() {
+        const selectedValue = this.value;
+        if (selectedValue == 'dark') {
+            document.body.classList.add('dark');
+            const logo = document.getElementById('byui-logo');
+            logo.src = './photos/byui-logo_white.webp';
+        } else {
+            document.body.classList.remove('dark');
+            const logo = document.getElementById('byui-logo');
+            logo.src = './photos/byui-logo_blue.webp';
+        }
+    });
+});
