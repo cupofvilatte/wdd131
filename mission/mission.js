1e7f43b3d388f4b1501ac611ab1d3b232e1553c4
cupofvilatte <kna22003@byui.edu>
2024-05-06 10:16:32 -0600

    j

--- mission/mission.js
+++ mission/mission.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', function () {
         if (selectedValue == 'dark') {
             document.body.classList.add('dark');
             const logo = document.getElementById('byui-logo');
-            logo.src = './byui-logo_white.png';
+            logo.src = './photos/byui-logo_white.png';
         } else {
             document.body.classList.remove('dark');
             const logo = document.getElementById('byui-logo');
-            logo.src = './byui-logo_blue.webp';
+            logo.src = './photos/byui-logo_blue.webp';
         }
     });
 });
